0d5b4701d3dd5829960d051302f4ed43900b2449
deadprogram <ron@hybridgroup.com>
2014-06-25 08:45:41 -0700

    Add example of reading temperature

---
+++ examples/read-temperature.js
@@ -0,0 +1,17 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: { 
+  	name: 'nest', 
+  	adaptor: 'nest', 
+  	token: 'XXX', 
+  	deviceId: 'ABC123'
+  },
+  device: {name: 'thermostat', driver: 'nest-thermostat'},
+
+  work: function(my) {
+    every((60).seconds(), function(){
+      console.log(my.thermostat.ambientTemperatureC());
+    });
+  }
+}).start();
