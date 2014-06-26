cbb37cb572c8a2a54afeb4826177ae962db39349
Edgar Silva <edgar.osc@gmail.com>
2014-06-26 17:14:35 -0500

    WIP on nest API: Fixes reading devices and emitting events when any value changes.

--- examples/read-temperature.js
+++ examples/read-temperature.js
@@ -1,17 +1,29 @@
 var Cylon = require('cylon');
 
 Cylon.robot({
-  connection: { 
-  	name: 'nest', 
-  	adaptor: 'nest', 
-  	accessToken: 'XXX', 
-  	deviceId: 'ABC123'
+  connection: {
+    name: 'nest',
+    adaptor: 'nest',
+    accessToken: 'c.1f7QA8se2tx5xOaKJYk2VUj3lkUiNClxhsbQczQiODTHUrHH4Sx0qEyN7K9kVw6NZ6xtMmyvTPpnEHXWPrTK4ZLygam5rJYH6tachF7Qw885DUHnsW9wiGXmaU8Q0yk68isxVExP4gPubB71'
   },
-  device: {name: 'thermostat', driver: 'nest-thermostat'},
+
+  device: {
+    name: 'thermostat',
+    driver: 'nest-thermostat',
+    deviceId: 'nGJBxgnS-Ow3u7xNXbUw1Of6ECoH41OD'
+  },
 
   work: function(my) {
-    every((60).seconds(), function(){
-      console.log(my.thermostat.ambientTemperatureC());
+
+    my.thermostat.on('value', function(data) {
+      console.log('The SnapShoot --->', data);
+    });
+
+    every((10).seconds(), function(){
+      console.log('NEST ambient temp C:', my.thermostat.ambientTemperatureC());
+      console.log('NEST ambient temp F:', my.thermostat.ambientTemperatureF());
+      console.log('NEST target temp F:', my.thermostat.targetTemperatureC());
+      console.log('NEST target temp F:', my.thermostat.targetTemperatureF());
     });
   }
 }).start();
