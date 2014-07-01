a53c6a96503fd1025a2f2c407f0599541748fe95
Edgar Silva <edgar.osc@gmail.com>
2014-07-01 13:32:43 -0500

    Updated nest home driver to make it easier to setup an structure id instead of passing it all the time.

--- examples/nest-home-away.js
+++ examples/nest-home-away.js
@@ -4,35 +4,34 @@ Cylon.robot({
   connection: {
     name: 'nest',
     adaptor: 'nest',
-    accessToken: 'c.1f7QA8se2tx5xOaKJYk2VUj3lkUiNClxhsbQczQiODTHUrHH4Sx0qEyN7K9kVw6NZ6xtMmyvTPpnEHXWPrTK4ZLygam5rJYH6tachF7Qw885DUHnsW9wiGXmaU8Q0yk68isxVExP4gPubB71'
+    accessToken: 'ACCESS_TOKEN'
   },
 
   device: {
     name: 'home',
-    driver: 'nest-home'
+    driver: 'nest-home',
+    structureId: 'STRUCTURE_ID'
   },
 
   work: function(my) {
 
     my.home.on('status', function(data) {
-      console.log('The Nest Home at a glance --->', data);
+      console.log('The Nest Home at a glance:', data);
     });
-
-    var sId = 'l38p6_vVeF9R7yl6GJgLuyHbEuVU_1ui7KAofiXXa5wZ9PBb-0AJaw';
 
     every((10).seconds(), function(){
       console.log('Nest home structures:', my.home.structures());
-      console.log('Nest home structure name:', my.home.structureName(sId));
-      console.log('Nest home thermostats:', my.home.thermostats(sId));
-      console.log('Nest home smokeCoAlarms:', my.home.smokeCoAlarms(sId));
-      console.log('Nest home away status:', my.home.away(sId));
-      console.log('Nest home eta:', my.home.eta(sId));
+      console.log('Nest home structure name:', my.home.structureName());
+      console.log('Nest home thermostats:', my.home.thermostats());
+      console.log('Nest home smokeCoAlarms:', my.home.smokeCoAlarms());
+      console.log('Nest home away status:', my.home.away());
+      console.log('Nest home eta:', my.home.eta());
 
       my.home.read('country_code', sId, function(data) {
         console.log("Nest home country id: ", data);
       });
 
-      my.home.away(sId, 'away');
+      my.home.away('away');
 
       /*
       my.home.write('away', 'home', sId, function(err) {
